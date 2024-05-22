baf275182585ecf60bee50dc7e0200293fe7a5a2
valeogre <104060142+valeogre@users.noreply.github.com>
2024-05-22 22:52:32 +0300

    Update stereo_to_mono.m

--- numerical_music/stereo_to_mono.m
+++ numerical_music/stereo_to_mono.m
@@ -1,5 +1,14 @@
 function mono = stereo_to_mono(stereo)
-  mono = stereo;
+  %mono = stereo;
+  [nr_samples, nr_channels] = size(stereo);
+
+  mono = zeros(nr_samples, 1);
+
+  % for i = 1:nr_samples
+  %  mono(i) = sum(stereo(i, :)) / nr_channels;
+  %endfor
+
+  mono = mean(stereo, 2);
 
   % Normalize
   mono = mono / max(abs(mono));
